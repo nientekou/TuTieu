0ed2b332349975b45f801d7a44d1d68e774342a9
nientekou <nientekou@gmail.com>
2026-09-04 00:45:31 +0700

    Update shop.js

--- src/commands/Economy/shop.js
+++ src/commands/Economy/shop.js
@@ -5,7 +5,7 @@ export default {
     slashOnly: true,
     data: new SlashCommandBuilder()
         .setName('shop')
-        .setDescription('Browse the economy shop.'),
+        .setDescription('Tiến vào Nhất Phẩm Các'),
 
     async execute(interaction, config, client) {
         return shopBrowse.execute(interaction, config, client);
