7c9ddc11edceb3705f4e3955dd6ad044f5ad4ab8
nientekou <nientekou@gmail.com>
2026-09-04 00:49:01 +0700

    Update shop-config.js

--- src/commands/Economy/shop-config.js
+++ src/commands/Economy/shop-config.js
@@ -5,15 +5,15 @@ export default {
     slashOnly: true,
     data: new SlashCommandBuilder()
         .setName('shop-config')
-        .setDescription('Configure shop settings. (Manage Server required)')
+        .setDescription('Thiết lập Nhất Phẩm Các. (Cần có quyền Quản Lý)')
         .addSubcommand(subcommand =>
             subcommand
                 .setName('setrole')
-                .setDescription('Set the Discord role granted when the Premium Role shop item is purchased.')
+                .setDescription('Thiết lập role khi vật phẩm từ Premium Role được mua')
                 .addRoleOption(option =>
                     option
                         .setName('role')
-                        .setDescription('The role to grant for Premium Role purchases.')
+                        .setDescription('Role bạn muốn cho Premium Role')
                         .setRequired(true),
                 ),
         ),
