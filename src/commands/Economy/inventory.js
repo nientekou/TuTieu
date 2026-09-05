105234b57f065b0a7b81eed0c3c801ae5437090a
nientekou <nientekou@gmail.com>
2026-09-05 15:51:27 +0700

    Update inventory.js

--- src/commands/Economy/inventory.js
+++ src/commands/Economy/inventory.js
@@ -40,15 +40,15 @@ export default {
             if (Object.keys(inventory).length > 0) {
                 inventoryDescription = Object.entries(inventory)
                     .filter(
-                        ([itemId, soluong]) => {
+                        ([itemId, quantity]) => {
                             const item = SHOP_ITEMS.find(i => i.id === itemId);
-                            return soluong > 0 && item;
+                            return quantity > 0 && item;
                         }
                     )
                     .map(
-                        ([itemId, soluong]) => {
+                        ([itemId, quantity]) => {
                             const item = SHOP_ITEMS.find(i => i.id === itemId);
-                            return `**${item.name}:** ${soluong}x`;
+                            return `**${item.name}:** ${quantity}x`;
                         }
                     )
                     .join("\n");
