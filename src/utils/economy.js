1f44a7a5efb71a29e3acfd57c534fe0a7de81330
nientekou <nientekou@gmail.com>
2026-09-07 23:22:52 +0700

    Update economy.js

--- src/utils/economy.js
+++ src/utils/economy.js
@@ -17,7 +17,7 @@ const WORK_MAX = ECONOMY_CONFIG.workMax || 100;
 const COOLDOWNS = ECONOMY_CONFIG.cooldowns || {
 daily: 24 * 60 * 60 * 1000,
 work: 60 * 60 * 1000,
-crime: 2 * 60 * 60 * 1000,
+phivu: 2 * 60 * 60 * 1000,
 rob: 4 * 60 * 60 * 1000,
 };
 
@@ -170,7 +170,7 @@ export function getWorkReward() {
     };
 }
 
-export function getCrimeOutcome() {
+export function getPhivuOutcome() {
     const outcomes = [
         {
             success: true,
