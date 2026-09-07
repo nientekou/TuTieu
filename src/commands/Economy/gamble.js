ce7f7a72bc1e2f89feb469c37b88d1ef47324e40
nientekou <nientekou@gmail.com>
2026-09-08 00:40:13 +0700

    Update gamble.js

--- src/commands/Economy/gamble.js
+++ src/commands/Economy/gamble.js
@@ -86,14 +86,14 @@ export default {
                 // Net change: the bet is replaced by the payout (bet was at stake, not pre-deducted)
                 cashChange = amountWon - betAmount;
 
-                resultEmbed = createEmbed(
+                resultEmbed = successEmbed(
                     "### <:idovan:1546543420569288816> THẮNG VẬN!",
                     `Đạo Hữu thắng Đổ Vận! **${betAmount.toLocaleString()}<:lt1:1545082415033360495>** đã thành **${amountWon.toLocaleString()}<:lt1:1545082415033360495>**!${tulinhthaoMessage}`,
                 );
             } else {
 cashChange = -betAmount;
 
-                resultEmbed = createEmbed(
+                resultEmbed = warningEmbed(
                     "### <:idovan:1546543420569288816> BẠI VẬN...",
                     `Thời vận không đứng về phía Đạo Hữu. Đã mất **${betAmount.toLocaleString()}<:lt1:1545082415033360495>**`,
                 );
