6b7134c07fceca7e231612befb593bebcb04a5af
nientekou <nientekou@gmail.com>
2026-09-07 23:41:33 +0700

    Update fish.js

--- src/commands/Economy/fish.js
+++ src/commands/Economy/fish.js
@@ -228,7 +228,7 @@ export default {
 
             const embed = createEmbed({
                 title: ':fishing_pole_and_fish: Câu Cá Thành Công!',
-                description: ` ${catchMessage}\nĐạo Hữu đã câu được **${fishCaught.emoji} ${fishCaught.name}**!\n${multiplierMessage}
+                description: ` ${catchMessage}\nĐạo Hữu đã câu được **${fishCaught.emoji} ${fishCaught.name}**!${multiplierMessage}
                 ### <:a1:1546550426063741058> THU HOẠCH
                 ㅤ└ ${finalEarned.toLocaleString()}<:lt1:1545082415033360495>`,
                 color: rarityColors[fishCaught.rarity]
