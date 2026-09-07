c861d0e488f54466a0a113c423362e6aad2caac4
nientekou <nientekou@gmail.com>
2026-09-07 20:52:29 +0700

    Update crime.js

--- src/commands/Economy/crime.js
+++ src/commands/Economy/crime.js
@@ -110,7 +110,7 @@ export default {
                 const embed = warningEmbed(
                     "⛓️ Phi Vụ Thất Bại!",
                     `${crime.name} bất thành! Đao Hữu đã bị **Chấp Pháp Ty** bắt giữ đem về **Lạc Tiên Uyên**` +
-                    `Bồi thường ${fine.toLocaleString()}<:lt1:1545082415033360495> và bị giam trong 1 canh giờ.`
+                    \N`Bồi thường ${fine.toLocaleString()}<:lt1:1545082415033360495> và bị giam trong 1 canh giờ.`
                 );
                 
                 await InteractionHelper.safeEditReply(interaction, { embeds: [embed] });
