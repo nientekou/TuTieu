ff394aa3c0ad01fc6e449626bb19035ae2c51c2a
nientekou <nientekou@gmail.com>
2026-09-07 21:29:00 +0700

    Update fish.js

--- src/commands/Economy/fish.js
+++ src/commands/Economy/fish.js
@@ -208,7 +208,7 @@ export default {
 
             if (hasCanthinhtrieu > 0) {
                 finalEarned = Math.floor(baseEarned * CANTHINHTRIEU_MULTIPLIER);
-                multiplierMessage = `\n🎣 **Thưởng thêm từ Cần Thính Triều: +50%**`;
+                multiplierMessage = `\n:fishing_pole_and_fish:  **Thưởng thêm từ Cần Thính Triều: +50%**`;
             }
 
             const catchMessage = CATCH_MESSAGES[Math.floor(Math.random() * CATCH_MESSAGES.length)];
@@ -227,8 +227,8 @@ export default {
             };
 
             const embed = createEmbed({
-                title: '🎣 Câu Cá Thành Công!',
-                description: `${catchMessage}\n\nĐạo Hữu đã câu được **${fishCaught.emoji} ${fishCaught.name}**! Thu hoạch được **${finalEarned.toLocaleString()}<:lt1:1545082415033360495>**!${multiplierMessage}`,
+                title: ':fishing_pole_and_fish:  Câu Cá Thành Công!',
+                description: `##${catchMessage}\nĐạo Hữu đã câu được **${fishCaught.emoji} ${fishCaught.name}**! Thu hoạch được **${finalEarned.toLocaleString()}<:lt1:1545082415033360495>**!${multiplierMessage}`,
                 color: rarityColors[fishCaught.rarity]
             })
                 .addFields(
