fe90ef41763350675e80e0db2e24c98ff5c0faf8
nientekou <nientekou@gmail.com>
2026-09-08 00:20:35 +0700

    Update mine.js

--- src/commands/Economy/mine.js
+++ src/commands/Economy/mine.js
@@ -149,10 +149,9 @@ userData.lastMine = now;
 
             const embed = successEmbed(
                 "<:icuoc:1545714179581943868> KHAI KHOÁNG THÀNH CÔNG!",
-                `Đạo Hữu đã thăm dò **${location}** ${multiplierMessage}
-                ### <:a1:1546550426063741058> THU HOẠCH
-                ㅤ└ ${finalEarned.toLocaleString()}<:lt1:1545082415033360495>
-                `,
+                 `Đạo Hữu đã thăm dò **${location}** ${multiplierMessage}
+                 ### <:a1:1546550426063741058> THU HOẠCH
+                ㅤ└${finalEarned.toLocaleString()}`,
             )
                 .addFields({
                     name: "<:lt1:1545082415033360495> Hiện Có",
