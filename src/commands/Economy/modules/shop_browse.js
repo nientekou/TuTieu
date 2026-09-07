35d92d7633c6040d746f4dae0efc09dab43c24a6
nientekou <nientekou@gmail.com>
2026-09-07 21:12:19 +0700

    Update shop_browse.js

--- src/commands/Economy/modules/shop_browse.js
+++ src/commands/Economy/modules/shop_browse.js
@@ -22,7 +22,7 @@ export default {
                 pageItems.forEach(item => {
                     embed.addFields({
                         name: `${item.name} (${item.id})`,
-                        value: `**Loại:** ${item.type}\n **Giá:** $${item.price.toLocaleString()}\n${item.description}`,
+                        value: `**Loại:** ${item.type}\n **Giá:** ${item.price.toLocaleString()}<:lt1:1545082415033360495>\n${item.description}`,
                         inline: false,
                     });
                 });
